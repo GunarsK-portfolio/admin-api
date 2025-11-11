8f980cadc22058d8fa76169302af66ea027761a2
Gunārs Kunakovs <gunarskunakovs@gmail.com>
2025-11-11 13:25:45 +0200

    Add config validations (#25)

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,42 +1,41 @@
 package config
 
-import "os"
+import (
+	"fmt"
+
+	"github.com/go-playground/validator/v10"
+
+	common "github.com/GunarsK-portfolio/portfolio-common/config"
+)
 
 type Config struct {
-	DBHost         string
-	DBPort         string
-	DBUser         string
-	DBPassword     string
-	DBName         string
-	Port           string
-	AuthServiceURL string
-	FilesAPIURL    string
+	DBHost         string `validate:"required"`
+	DBPort         string `validate:"required,number,min=1,max=65535"`
+	DBUser         string `validate:"required"`
+	DBPassword     string `validate:"required"`
+	DBName         string `validate:"required"`
+	Port           string `validate:"required,number,min=1,max=65535"`
+	AuthServiceURL string `validate:"required,url"`
+	FilesAPIURL    string `validate:"required,url"`
 }
 
 func Load() *Config {
-	return &Config{
-		DBHost:         getEnvRequired("DB_HOST"),
-		DBPort:         getEnvRequired("DB_PORT"),
-		DBUser:         getEnvRequired("DB_USER"),
-		DBPassword:     getEnvRequired("DB_PASSWORD"),
-		DBName:         getEnvRequired("DB_NAME"),
-		Port:           getEnv("PORT", "8083"),
-		AuthServiceURL: getEnvRequired("AUTH_SERVICE_URL"),
-		FilesAPIURL:    getEnvRequired("FILES_API_URL"),
+	cfg := &Config{
+		DBHost:         common.GetEnvRequired("DB_HOST"),
+		DBPort:         common.GetEnvRequired("DB_PORT"),
+		DBUser:         common.GetEnvRequired("DB_USER"),
+		DBPassword:     common.GetEnvRequired("DB_PASSWORD"),
+		DBName:         common.GetEnvRequired("DB_NAME"),
+		Port:           common.GetEnv("PORT", "8083"),
+		AuthServiceURL: common.GetEnvRequired("AUTH_SERVICE_URL"),
+		FilesAPIURL:    common.GetEnvRequired("FILES_API_URL"),
 	}
-}
 
-func getEnv(key, defaultValue string) string {
-	if value := os.Getenv(key); value != "" {
-		return value
+	// Validate configuration
+	validate := validator.New()
+	if err := validate.Struct(cfg); err != nil {
+		panic(fmt.Sprintf("Invalid configuration: %v", err))
 	}
-	return defaultValue
-}
 
-func getEnvRequired(key string) string {
-	value := os.Getenv(key)
-	if value == "" {
-		panic("Required environment variable " + key + " is not set")
-	}
-	return value
+	return cfg
 }
